3bb933cb0ed4975c4affa0f4f95ce428b06bd287
shivbapatil19 <patilshivba03@gmail.com>
2025-08-25 02:59:25 -0700

    Add files via upload

---
+++ GithubDAY1.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#first program of python
+print("Hello World")
+
+
+# In[2]:
+
+
+print("GitHub second code")
+
+
+# In[3]:
+
+
+#addition of the 2 numbers
+a=5
+b=4
+c=a+b
+print(c)
+
+
+# In[4]:
+
+
+#substraction of the 2 numbers
+a=6
+b=4
+c=a-b
+print(c)
+
+
+# In[5]:
+
+
+#taking the input from the user for addition
+a=int(input("enter the number you want to enter"))
+b=int(input("enter the number you want to enter"))
+c=a+b
+print(c)
+
+
+# In[6]:
+
+
+#taking the input from the user for multiplication
+a=int(input("enter the number you want to enter"))
+b=int(input("enter the number you want to enter"))
+c=a*b
+print(c)
+
+
+# In[8]:
+
+
+#taking the input from the user for division
+a=int(input("enter the number you want to enter"))
+b=int(input("enter the number you want to enter"))
+c=a/b
+print(c)
+
+
+# In[11]:
+
+
+#taking the input from the user for square
+a=int(input("enter the number you want to enter"))
+c=a*a
+print(c)
+
+
+# In[ ]:
+
+
+
+
